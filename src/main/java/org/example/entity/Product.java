9d6a103df8500f6d4bdbb9ba3ebfc4b31beb80ef
AshrafRasulov <ashee87.news@gmail.com>
2024-11-21 00:03:41 +0500

    Lesson 01 result

---
+++ src/main/java/org/example/entity/Product.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Short amount;
+}
